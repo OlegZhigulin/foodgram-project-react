770215204e94b2a5cf8c184d10ebe75e8a0c45d1
Олег Жигулин <olezhonok94@gmail.com>
2022-10-11 02:22:46 +0300

    fix admin panel

--- backend/foodgram/users/rus_token.py
+++ backend/foodgram/users/rus_token.py
@@ -3,6 +3,5 @@ from rest_framework.authtoken.models import Token
 
 class RusToken(Token):
     class Meta:
-        proxy = True
-        verbose_name = _("ТОКЕН")
-        verbose_name_plural = _("ТОКЕНЫ")
+        verbose_name = 'ТОКЕН'
+        verbose_name_plural = 'ТОКЕНЫ'
