ac790894601604eb171e9f1fb989ff37d6b46bfa
Олег Жигулин <olezhonok94@gmail.com>
2022-10-10 23:26:53 +0300

    fix admin panel

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from rest_framework.authtoken.admin import TokenProxy
+from rest_framework.authtoken import admin
 
 
 class CustomUser(AbstractUser):
@@ -76,7 +76,8 @@ class Subscribe(models.Model):
         return f'{self.user} подписан на {self.author}'
 
 
-class RusTokenProxy(TokenProxy):
+class RusTokenProxy(admin.TokenProxy):
     class Meta:
-        verbose_name = 'ТОКЕНЫ'
         proxy = True
+        verbose_name = 'ТОКЕН'
+        verbose_name_plural = 'ТОКЕНЫ'
